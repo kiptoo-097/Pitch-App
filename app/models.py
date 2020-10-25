138f493733e6b23a9427d73ca6f50e370e3c144d
kiptoo-097 <kiptoo097.com>
2020-10-25 23:39:11 +0300

    added pass secure for authentication

--- app/models.py
+++ app/models.py
@@ -7,7 +7,8 @@ class User(db.Model):
     email = db.Column(db.String(255),unique = True,index = True)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
-    
+    pass_secure = db.Column(db.String(255))
+
 
     def __repr__(self):
         return f'User {self.username}'

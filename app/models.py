de4164c185103793a0731e365936aaf4e2cd87f5
kiptoo-097 <kiptoo097.com>
2020-10-25 23:38:05 +0300

    database moigration

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from . import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    
+
+    def __repr__(self):
+        return f'User {self.username}'
+
---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Kamunguna1@localhost/pitch'
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}

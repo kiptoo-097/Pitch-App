dddafe062e67704541effd02154b0380fa4d7da2
kiptoo-097 <kiptoo097.com>
2020-10-29 19:47:43 +0300

    added views.py to authentication folder

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,10 @@
-from flask import render_template
+from flask import render_template,redirect,url_for, flash,request
+from flask_login import login_user,logout_user,login_required
 from . import auth
+from ..models import User
+from .forms import LoginForm,RegistrationForm
+from .. import db
+from ..emails import mail_message
 
-@auth.route('/login')
-def login():
-    return render_template('auth/login.html')
+
+

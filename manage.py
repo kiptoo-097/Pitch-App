2893dc35b815e23311188d011ffc84d5fdbbad0b
kiptoo-097 <kiptoo097.com>
2020-10-29 18:58:38 +0300

    Imported class user class to manage

--- manage.py
+++ manage.py
@@ -1,17 +1,6 @@
 from app import create_app,db
 from app.models import User
-from flask_script import Manager,Server
-from  flask_migrate import Migrate, MigrateCommand
 
-# Creating app instance
-app = create_app('development')
-
-manager = Manager(app)
-manager.add_command('server',Server)
-
-
-migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
 
 
 @manager.shell

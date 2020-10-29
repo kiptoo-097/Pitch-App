9e4b8a7c9473769c7c33c60effeb6646036ab31d
kiptoo-097 <kiptoo097.com>
2020-10-29 20:16:12 +0300

    Added class comment to models.py

--- app/models.py
+++ app/models.py
@@ -78,5 +78,21 @@ class Pitch(db.Model):
 
         return pitches_count
 
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String(1000))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    pitch = db.Column(db.Integer,db.ForeignKey("pitches.id"))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,pitch):
+        comments = Comment.query.filter_by(pitch_id=pitch).all()
+        return comments
 
 
--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 export MAIL_USERNAME='kiptookiptooken@gmail.com'
 export MAIL_PASSWORD='Moringa1'
 export SECRET_KEY='kiptoo'
+
+
+python3.8 manage.py server

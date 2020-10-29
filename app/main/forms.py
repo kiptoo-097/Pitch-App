c6862a776045d2ef490f928631042eadab81ee1c
kiptoo-097 <kiptoo097.com>
2020-10-29 15:42:22 +0300

    Added class pitchForm

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,3 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField,SelectField
 from wtforms.validators import Required
 
+class PitchForm(FlaskForm):
+
+    title = StringField('Pitch title',validators=[Required()])
+    text = TextAreaField('Text',validators=[Required()])
+    category = SelectField('Type',choices=[('interview','Interview pitch'),('product','Product pitch'),('promotion','Promotion pitch')],validators=[Required()])
+    submit = SubmitField('Submit')
+
